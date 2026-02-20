ee86624d3559a2523ea2da8332f833f1b95902d9
FreeOnMinute <yarkinmakar@yandex.ru>
2026-02-21 01:21:12 +0300

    first commit

--- src/Post.jsx
+++ src/Post.jsx
@@ -5,9 +5,9 @@ export default function Post({ title, description, likes, date, image }) {
     <div className="post">
       <h3>{title}</h3>
       
-      {true && (
+      {image && (
         <div className="post-image">
-          <img src={"https://picsum.photos/600/400" } alt={title} />
+          <img src={image } alt={title} />
         </div>
       )}
       
